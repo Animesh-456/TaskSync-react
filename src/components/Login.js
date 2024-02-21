beec3940b591c9e491975512711352715c5a4a0c
Animesh-456 <anim29006@gmail.com>
2024-02-21 22:17:56 +0530

    Fixed responsive view for login and register page

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Login = () => {
   return (
     <>
-      <div className='log-container'>
+      {/* <div className='log-container'>
         <div className='form-box'>
           <div className="top">
             <h2>Don't have an account? <Link to="/signup" onclick="register()">Sign Up</Link></h2>
@@ -25,6 +25,52 @@ const Login = () => {
             <button className='login-btn'>Login</button>
           </div>
         </div>
+      </div> */}
+
+      {/* <div className='login-cont'>
+        <div className='box-log'>
+          <div className='top'>
+            <h3>Login <span style={{ color: 'red' }}> T</span>ask<span style={{ color: 'red' }}>S</span>ync</h3>
+          </div>
+
+
+          <div className='top2'>
+            <div className="input-box">
+              <input type="text" className="input-field" placeholder="Username or Email" />
+
+            </div>
+            <div className="input-box">
+              <input type="password" className="input-field" placeholder="Password" />
+
+            </div>
+            <div className='log-btn-cont'>
+              <button className='log-btn'>Login</button>
+            </div>
+          </div>
+
+
+        </div>
+      </div> */}
+
+
+      <div className="form-cont">
+        <h2>Login to <span style={{ color: 'red' }}> T</span>ask<span style={{ color: 'red' }}>S</span>ync</h2>
+        <form>
+          <label htmlFor="name">Email/Username:</label>
+          <input placeholder='Username or Email' type="email" id="email" name="email" />
+
+          <label htmlFor="name">Type Of account</label>
+          <select id="account-type">
+            <option value="Assigner">Assigner</option>
+            <option value="Employee">Employee</option>
+          </select>
+
+          <label htmlFor="email">Password:</label>
+          <input placeholder="Password" type="password" id="password" name="password" />
+
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </>
   )
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,37 +2,70 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const Signup = () => {
     return (
-        <div className='log-container2'>
-            <div className='form-box'>
-                <div className="top">
-                    <h2>Register to <span style={{ color: 'red' }}> T</span>ask<span style={{ color: 'red' }}>S</span>ync</h2>
-                    {/* <h3>Signup to TaskSync</h3> */}
+        // <div className='log-container2'>
+        //     <div className='form-box'>
+        //         <div className="top">
+        //             <h2>Register to <span style={{ color: 'red' }}> T</span>ask<span style={{ color: 'red' }}>S</span>ync</h2>
+        //             {/* <h3>Signup to TaskSync</h3> */}
+        //         </div>
+
+        //         <div className="input-box">
+        //             <input type="text" className="input-field" placeholder="Username or Email" />
+        //             <i className="bx bx-user"></i>
+        //         </div>
+
+        //         <div className="input-box">
+        //             <input type="password" className="input-field" placeholder="Password" />
+        //             <i className="bx bx-user"></i>
+        //         </div>
+
+
+        //         <div className="input-box">
+        //             {/* <label for="cars">Choose a car:</label> */}
+
+        //             <select id="cars">
+        //                 <option value="Assigner">Assigner</option>
+        //                 <option value="Employee">Employee</option>
+        //             </select>
+        //         </div>
+
+        //         <div className='login-submit-container'>
+        //             <button className='login-btn'>SignUp</button>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className="form-cont2">
+            <h2>Register to <span style={{ color: 'red' }}> T</span>ask<span style={{ color: 'red' }}>S</span>ync</h2>
+            <form>
+                <div className='f-l-name'>
+                    {/* <div> */}
+                        <div className='f-l-child'>
+                            <label htmlFor="name">First Name</label>
+                            <input placeholder='First name' type="text" id="fname" name="fname" />
+                        </div>
+                    {/* </div> */}
+                    {/* <div> */}
+                        <div className='f-l-child'>
+                            <label htmlFor="name">Last Name</label>
+                            <input placeholder='Last Name' type="text" id="lname" name="lname" />
+                        </div>
+                    {/* </div> */}
                 </div>
+                <label htmlFor="name">Email</label>
+                <input placeholder='Email' type="email" id="email" name="email" />
 
-                <div className="input-box">
-                    <input type="text" className="input-field" placeholder="Username or Email" />
-                    <i className="bx bx-user"></i>
-                </div>
+                <label htmlFor="name">Type Of account</label>
+                <select id="account-type">
+                    <option value="Assigner">Assigner</option>
+                    <option value="Employee">Employee</option>
+                </select>
 
-                <div className="input-box">
-                    <input type="password" className="input-field" placeholder="Password" />
-                    <i className="bx bx-user"></i>
-                </div>
+                <label htmlFor="email">Password:</label>
+                <input placeholder="Password" type="password" id="password" name="password" />
 
-
-                <div className="input-box">
-                    {/* <label for="cars">Choose a car:</label> */}
-
-                    <select id="cars">
-                        <option value="Assigner">Assigner</option>
-                        <option value="Employee">Employee</option>
-                    </select>
-                </div>
-
-                <div className='login-submit-container'>
-                    <button className='login-btn'>SignUp</button>
-                </div>
-            </div>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
