08c43e45048ea91a4bcbef2e71243cd53e28f82b
Animesh-456 <anim29006@gmail.com>
2025-02-13 11:31:08 +0530

    changes backend links2

--- src/common/links.js
+++ src/common/links.js
@@ -1,7 +1,7 @@
 const frontendroute = 'http://localhost:3000';
 
-const backendFileroute = 'http://http://20.40.44.161/:4000/uploads'
+const backendFileroute = 'http://20.40.44.161/:4000/uploads'
 
-const backendURL = 'http://http://20.40.44.161:4000'
+const backendURL = 'http://20.40.44.161:4000'
 
 export { frontendroute, backendFileroute, backendURL }
